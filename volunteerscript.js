38938e3e8e8387c2c4815221e5fee5a9339a0105
RachidAzrou <razrou@outlook.be>
2024-05-20 15:04:34 +0200

    Update volunteerscript.js

--- volunteerscript.js
+++ volunteerscript.js
@@ -1,23 +1,15 @@
 document.getElementById("okButton").addEventListener("click", function() {
-    // Schakel groen licht in
     document.getElementById("greenLight").classList.add("on");
     document.getElementById("redLight").classList.remove("on");
-    // Stuur statusupdate naar de Imam
-    updateImamStatus("Groen licht: Aan");
+    receiveVolunteerStatus("Groen licht: Aan");
 });
 
 document.getElementById("nokButton").addEventListener("click", function() {
-    // Schakel rood licht in
     document.getElementById("redLight").classList.add("on");
     document.getElementById("greenLight").classList.remove("on");
-    // Stuur statusupdate naar de Imam
-    updateImamStatus("Rood licht: Aan");
+    receiveVolunteerStatus("Rood licht: Aan");
 });
 
-// Functie om statusupdate naar de Imam te sturen
-function updateImamStatus(status) {
-    // Stuur statusupdate naar de Imam
-    // Hier kun je logica toevoegen om de status naar de backend te sturen of naar een andere pagina te navigeren
-    // Voor nu zullen we de status eenvoudigweg afdrukken in de console
+function receiveVolunteerStatus(status) {
     console.log("Statusupdate naar de Imam:", status);
 }
