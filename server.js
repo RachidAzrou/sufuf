c922de27f569af6abd4a207e57645c46c3e3b65b
RachidAzrou <razrou@outlook.be>
2024-05-21 22:56:22 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,28 @@
 const express = require('express');
-const WebSocket = require('ws');
 const http = require('http');
 const path = require('path');
+const socketIO = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+const io = socketIO(server);
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-wss.on('connection', ws => {
-    ws.on('message', message => {
-        // Stuur het bericht door naar alle verbonden clients (inclusief de imam)
-        wss.clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
+// WebSocket event handlers
+io.on('connection', socket => {
+    console.log('A user connected');
+
+    // Handle messages received from clients
+    socket.on('message', data => {
+        // Broadcast the message to all connected clients
+        io.emit('message', data);
+    });
+
+    // Handle disconnect event
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
     });
 });
 
