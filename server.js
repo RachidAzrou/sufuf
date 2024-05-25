45ead3778cad136ab21e10dc63a7bf1bdd3036d6
RachidAzrou <razrou@outlook.be>
2024-05-25 17:53:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 // WebSocket event handlers
 io.on('connection', socket => {
     console.log('A user connected');
-    
+
     socket.on('message', data => {
         // Broadcast the message to all connected clients
         io.emit('message', data);
@@ -11,4 +23,9 @@ io.on('connection', socket => {
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
